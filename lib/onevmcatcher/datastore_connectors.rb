a991e359340431f6c5c38a967cc0a1a2a9655d61
kosoburak <kosoburak@gmail.com>
2015-08-24 09:54:37 +0200

    datastore connectors deleted

--- lib/onevmcatcher/datastore_connectors.rb
+++
@@ -1,8 +0,0 @@
-# Wrapper for all available datastore connectors.
-module Onevmcatcher::DatastoreConnectors; end
-
-# Load base datastore connector first
-require File.join(File.dirname(__FILE__), 'datastore_connectors', "base_datastore_connector")
-
-# Load all available datastore connectors
-Dir.glob(File.join(File.dirname(__FILE__), 'datastore_connectors', "*.rb")) { |connector_file| require connector_file.chomp('.rb') }
