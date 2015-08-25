95b83abbc285772e222db9f64dcc4baa03fbee16
kosoburak <kosoburak@gmail.com>
2015-08-25 10:40:50 +0200

    method_missing implementation

--- lib/onevmcatcher/vmcatcher_configuration.rb
+++ lib/onevmcatcher/vmcatcher_configuration.rb
@@ -2,6 +2,23 @@ module Onevmcatcher
   # Wraps vmcatcher configuration meta data.
   class VmcatcherConfiguration < VmcatcherEnv
 
+    # Known methods names used by this class
+    KNOWN_METHOD_NAMES = [
+      'rdbms',
+      'cache_event',
+      'log_conf',
+      'dir_cert',
+      'cache_dir_cache',
+      'cache_dir_download',
+      'cache_dir_expire',
+      'cache_action_download',
+      'cache_action_check',
+      'cache_action_expire'
+    ].freeze
+
+    # Prefix for making vmcatcher attributes
+    VMCATCHER_ATTR_PREFIX = "VMCATCHER_"
+
     # Known vmcatcher configuration attributes
     REGISTERED_ENV_KEYS = [
       'VMCATCHER_RDBMS',
@@ -16,14 +33,33 @@ module Onevmcatcher
       'VMCATCHER_CACHE_ACTION_EXPIRE'
     ].freeze
 
-    REGISTERED_ENV_KEYS.each do |registered_env_key|
-      short_env_key = registered_env_key.gsub(/^VMCATCHER_/, '')
+    #    REGISTERED_ENV_KEYS.each do |registered_env_key|
+    #      short_env_key = registered_env_key.gsub(/^VMCATCHER_/, '')
 
-      class_eval %Q|
-def #{short_env_key.downcase}
-  attributes["#{registered_env_key}"]
-end
-|
+    #      class_eval %Q|
+    #def #{short_env_key.downcase}
+    #  attributes["#{registered_env_key}"]
+    #end
+    #|
+    #    end
+    def method_missing(method_id, *arguments, &block)
+      if KNOWN_METHOD_NAMES.include? method)id.to_s
+        self.class.send :define_method, method_id do
+          temp = VMCATCHER_ATTR_PREFIX + method_id
+          attributes["#{temp}"]
+        end
+        self.send(method_id)
+      else
+        super
+      end
+    end
+
+    def respond_to?(method_id, include_private = false)
+      if KNOWN_METHOD_NAMES.include? method_id.to_s
+        true
+      else
+        super
+      end
     end
 
   end
--- lib/onevmcatcher/vmcatcher_event.rb
+++ lib/onevmcatcher/vmcatcher_event.rb
@@ -2,6 +2,31 @@ module Onevmcatcher
   # Wraps vmcatcher event meta data.
   class VmcatcherEvent < VmcatcherEnv
 
+    # Known methods names used by this class
+    KNOWN_METHOD_NAMES = [
+      'type',
+      'dc_description',
+      'dc_identifier',
+      'dc_title',
+      'hv_hypervisor',
+      'hv_size',
+      'hv_uri',
+      'hv_format',
+      'hv_version',
+      'sl_arch',
+      'sl_checksum_sha512',
+      'sl_comments',
+      'sl_os',
+      'sl_osversion',
+      'il_dc_identifier',
+      'ad_mpuri',
+      'filename',
+      'vo'
+    ].freeze
+
+    # Prefix for making vmcatcher attributes
+    VMCATCHER_ATTR_PREFIX = "VMCATCHER_EVENT_"
+
     # Known event attributes used by vmcatcher
     REGISTERED_ENV_KEYS = [
       'VMCATCHER_EVENT_TYPE',
@@ -24,15 +49,36 @@ module Onevmcatcher
       'VMCATCHER_EVENT_VO'
     ].freeze
 
-    REGISTERED_ENV_KEYS.each do |registered_env_key|
-      short_env_key = registered_env_key.gsub(/^VMCATCHER_EVENT_/, '')
+    #   REGISTERED_ENV_KEYS.each do |registered_env_key|
+    #     short_env_key = registered_env_key.gsub(/^VMCATCHER_EVENT_/, '')
 
-      class_eval %Q|
-def #{short_env_key.downcase}
-  attributes["#{registered_env_key}"]
-end
-|
+    #      class_eval %Q|
+    #def #{short_env_key.downcase}
+    #  attributes["#{registered_env_key}"]
+    #end
+    #|
+    #    end
+
+    def method_missing(method_id, *arguments, &block)
+      if KNOWN_METHOD_NAMES.include? method_id.to_s
+        self.class.send :define_method, method_id do
+          temp = VMCATCHER_ATTR_PREFIX + method_id
+          attributes["#{temp}"]
+        end
+        self.send(method_id)
+      else
+        super
+      end
     end
+
+    def respond_to?(method_id, include_private = false)
+      if KNOWN_METHOD_NAMES.include? method_id.to_s
+        true
+      else
+        super
+      end
+    end
+
 
   end
 end
