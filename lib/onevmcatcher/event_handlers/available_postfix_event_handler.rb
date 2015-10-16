4e6b36327eef77bfc76b4b105df8bc6cdbb70b9b
kosoburak <kosoburak@gmail.com>
2015-10-16 19:10:40 +0200

    Added descriptor creation for new / editit image

--- lib/onevmcatcher/event_handlers/available_postfix_event_handler.rb
+++ lib/onevmcatcher/event_handlers/available_postfix_event_handler.rb
@@ -7,8 +7,28 @@ module Onevmcatcher::EventHandlers
       Onevmcatcher::Log.info "[#{self.class.name}] Handling updated image " \
                              "for #{vmcatcher_event.dc_identifier.inspect}"
 
+      create_descriptor(vmcatcher_event)
       image_transformer_instance = Onevmcatcher::ImageTransformer.new()
       image_transformer_instance.transform!(vmcatcher_event,vmcatcher_configuration)
+    end
+
+    # Create appliance descriptor from VMCATCHER_EVENT metadata.
+    def create_descriptor(metadata)
+      os = Cloud::Appliance::Descriptor::Os.new(:distribution => metadata.sl_osversion,
+                                                :version => metadata.sl_osversion)
+      disk = Cloud::Appliance::Descriptor::Disk.new(:type => :os,
+                                                    :format => options.required_format)
+
+      appliance = Cloud::Appliance::Descriptor::Appliance.new :action => create
+      appliance.title = metadata.dc_title
+      appliance.version = metadata.hv_version
+      appliance.os = os
+      appliance.add_group metadata.vo 
+      appliance.description = metadata.dc_title
+      appliance.identifier = metadata.dc_identifier
+      appliance.attributes = metadata.to_hash
+
+      descriptor = appliance.to_json
     end
 
   end
