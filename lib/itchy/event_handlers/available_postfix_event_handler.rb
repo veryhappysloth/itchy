bd74f7ea5ec910ce9a42f7e82ce60a30a81a4268
kosoburak <kosoburak@gmail.com>
2016-01-11 13:37:51 +0100

    Refactored event handler for available_postfix. Moved image transformation before descriptor creation, changed file name in path of descriptor

--- lib/itchy/event_handlers/available_postfix_event_handler.rb
+++ lib/itchy/event_handlers/available_postfix_event_handler.rb
@@ -10,9 +10,9 @@ module Itchy::EventHandlers
       Itchy::Log.info "[#{self.class.name}] Handling updated image " \
                              "for #{vmcatcher_event.dc_identifier.inspect}"
       begin
-        save_descriptor(create_descriptor(vmcatcher_event), event_name)
         image_transformer_instance = Itchy::ImageTransformer.new(@options)
         new_file_name = image_transformer_instance.transform!(vmcatcher_event, vmcatcher_configuration)
+        save_descriptor(create_descriptor(vmcatcher_event, file_name), event_name)
       rescue Itchy::Errors::PrepareEnvError, ArgumentError, Itchy::Errors::ImageTransformationError  => ex
         Itchy::Log.error "[#{self.class.name}] Problem with handling event #{event_name}" \
           "Event handling failed with #{ex.message}"
@@ -26,12 +26,12 @@ module Itchy::EventHandlers
     #
     # @param metadata [Itchy::VmcatcherEvent] vmcatcher event to get metadata from
     # @return [String] json form of created descriptor
-    def create_descriptor(metadata)
+    def create_descriptor(metadata, file_name)
       os = ::Cloud::Appliance::Descriptor::Os.new(distribution: metadata.sl_osversion,
                                                   version: metadata.sl_osversion)
       disk = ::Cloud::Appliance::Descriptor::Disk.new(type: :os,
                                                       format: @options.required_format,
-                                                      path: "#{@options.output_dir}/#{metadata.dc_identifier}")
+                                                      path: "#{@options.output_dir}/#{file_name}")
 
       appliance = ::Cloud::Appliance::Descriptor::Appliance.new action: :registration
       appliance.title = metadata.dc_title
